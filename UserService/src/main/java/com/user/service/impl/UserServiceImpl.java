c3f834ca4a752f4abf90e1acaf13a2dc313ae812
ManishPradhan1 <manishpradhan37965@gmail.com>
2026-05-08 04:10:10 +0530

    Initial commit — Hotel Microservices

---
+++ UserService/src/main/java/com/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.user.service.impl;
+
+import com.user.service.entities.Hotel;
+import com.user.service.entities.Rating;
+import com.user.service.entities.User;
+import com.user.service.exceptions.ResourceNotFoundException;
+import com.user.service.repositories.UserRepository;
+import com.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Override
+    public User saveUser(User user) {
+
+        //generate unique userid
+        String randomUserId = UUID.randomUUID().toString();
+        user.setUserId(randomUserId);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        //implement RATING SERVICE CALL: USING REST TEMPLATE
+        return userRepository.findAll();
+    }
+
+    //get single user
+    @Override
+    public User getUser(String userId) {
+        //get user from database with the help of user repository
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : " + userId));
+
+        //fetch rating of the above user from rating service
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATINGSERVICE/ratings/users/" + user.getUserId(), Rating[].class);
+        logger.info("{} ", ratingsOfUser);
+
+        List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            //api call to hotel service to get the hotel
+            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTELSERVICE/hotels/" + rating.getHotelId(), Hotel.class);
+            Hotel hotel = forEntity.getBody();
+            logger.info("response stat0us code: {} ", forEntity.getStatusCode());
+
+            //set the hotel to rating
+            rating.setHotel(hotel);
+
+            //return the rating
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingList);
+        return user;
+    }
+}
---
+++ UserService/src/main/java/com/user/service/services/UserService.java
@@ -0,0 +1,20 @@
+package com.user.service.services;
+
+import com.user.service.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    //User operations
+
+    //create
+    User saveUser(User user);
+
+    //get all user
+    List<User> getAllUser();
+
+    //get single user of given userId
+    User getUser(String userId);
+
+}
